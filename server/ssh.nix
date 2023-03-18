d379d9feb33ddb91bf60ebb6083c02da23589b84
rtkt <rtkt@protonmail.com>
2023-03-18 14:47:41 +0700

    Tuned SSHGuard a bit more aggressive on the server

--- server/ssh.nix
+++ server/ssh.nix
@@ -7,4 +7,14 @@
     Host localhost
       ForwardAgent yes
   '';
+
+  services.sshguard = {
+    blacklist_threshold = 60;
+    detection_time = 4800;
+    whitelist = [
+      "45.57.90.45"
+      "185.91.179.79"
+      "195.209.97.211"
+    ];
+  };
 }

1e10935e10ba3814b1631ed8ebc89375b575243c
rtkt <rtkt@protonmail.com>
2023-03-28 22:13:23 +0700

    Deleted Pipewire settings

--- desktop/sound.nix
+++ desktop/sound.nix
@@ -1,12 +1,7 @@
 {
   sound.enable = true;
-  # hardware.pulseaudio.enable = true;
   services.pipewire = {
     enable = true;
     pulse.enable = true;
-    config = {
-      pipewire.resample.quality = 10;
-      pipewire-pulse.resample.quality = 10;
-    };
   };
 }

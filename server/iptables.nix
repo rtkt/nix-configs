da35ebbec09c0ed556452954a78d26b4992c8d41
rtkt <rtkt@protonmail.com>
2023-06-02 10:30:20 +0700

    Rewrote server/iptables.nix

--- server/iptables.nix
+++ server/iptables.nix
@@ -1,36 +1,33 @@
-{
+{ lib,  ... }:
+let
+  f = ipList:
+    let
+      command = ip: port: "iptables -A INPUT -s ${ip} -p tcp --dport ${toString port} -j ACCEPT\n";
+      acceptIp = ip: map (command ip) cloudflarePorts;
+      generateCommands = targets: map acceptIp ipList;
+    in lib.concatStrings (lib.flatten  (map acceptIp ipList));
+  
+  cloudflarePorts = [ 80 443 ];
+  cloudflareIps = [
+    "103.21.244.0/22"
+    "103.22.200.0/22"
+    "103.31.4.0/22"
+    "104.16.0.0/12"
+    "108.162.192.0/18"
+    "131.0.72.0/22"
+    "141.101.64.0/18"
+    "162.158.0.0/15"
+    "172.64.0.0/13"
+    "173.246.48.0/20"
+    "188.114.96.0/20"
+    "190.93.240.0/20"
+    "197.234.240.0/22"
+    "198.41.128.0/17"
+  ];
+in{
   networking.firewall = {
     allowedTCPPorts = [22222];
     logRefusedConnections = false;
-    extraCommands = ''
-      iptables -A INPUT -s 103.21.244.0/22 -p tcp --dport 80 -j ACCEPT &&
-      iptables -A INPUT -s 103.22.200.0/22 -p tcp --dport 80 -j ACCEPT &&
-      iptables -A INPUT -s 103.31.4.0/22 -p tcp --dport 80 -j ACCEPT &&
-      iptables -A INPUT -s 104.16.0.0/12 -p tcp --dport 80 -j ACCEPT &&
-      iptables -A INPUT -s 108.162.192.0/18 -p tcp --dport 80 -j ACCEPT &&
-      iptables -A INPUT -s 131.0.72.0/22 -p tcp --dport 80 -j ACCEPT &&
-      iptables -A INPUT -s 141.101.64.0/18 -p tcp --dport 80 -j ACCEPT &&
-      iptables -A INPUT -s 162.158.0.0/15 -p tcp --dport 80 -j ACCEPT &&
-      iptables -A INPUT -s 172.64.0.0/13 -p tcp --dport 80 -j ACCEPT &&
-      iptables -A INPUT -s 173.245.48.0/20 -p tcp --dport 80 -j ACCEPT &&
-      iptables -A INPUT -s 188.114.96.0/20 -p tcp --dport 80 -j ACCEPT &&
-      iptables -A INPUT -s 190.93.240.0/20 -p tcp --dport 80 -j ACCEPT &&
-      iptables -A INPUT -s 197.234.240.0/22 -p tcp --dport 80 -j ACCEPT &&
-      iptables -A INPUT -s 198.41.128.0/17 -p tcp --dport 80 -j ACCEPT &&
-      iptables -A INPUT -s 103.21.244.0/22 -p tcp --dport 443 -j ACCEPT &&
-      iptables -A INPUT -s 103.22.200.0/22 -p tcp --dport 443 -j ACCEPT &&
-      iptables -A INPUT -s 103.31.4.0/22 -p tcp --dport 443 -j ACCEPT &&
-      iptables -A INPUT -s 104.16.0.0/12 -p tcp --dport 443 -j ACCEPT &&
-      iptables -A INPUT -s 108.162.192.0/18 -p tcp --dport 443 -j ACCEPT &&
-      iptables -A INPUT -s 131.0.72.0/22 -p tcp --dport 443 -j ACCEPT &&
-      iptables -A INPUT -s 141.101.64.0/18 -p tcp --dport 443 -j ACCEPT &&
-      iptables -A INPUT -s 162.158.0.0/15 -p tcp --dport 443 -j ACCEPT &&
-      iptables -A INPUT -s 172.64.0.0/13 -p tcp --dport 443 -j ACCEPT &&
-      iptables -A INPUT -s 173.245.48.0/20 -p tcp --dport 443 -j ACCEPT &&
-      iptables -A INPUT -s 188.114.96.0/20 -p tcp --dport 443 -j ACCEPT &&
-      iptables -A INPUT -s 190.93.240.0/20 -p tcp --dport 443 -j ACCEPT &&
-      iptables -A INPUT -s 197.234.240.0/22 -p tcp --dport 443 -j ACCEPT &&
-      iptables -A INPUT -s 198.41.128.0/17 -p tcp --dport 443 -j ACCEPT &&
-    '';
+    extraCommands = f cloudflareIps;
   };
 }

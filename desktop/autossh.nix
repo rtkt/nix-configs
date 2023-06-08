87378acf1bb5d35cd507b410efdb76b9941f00dd
rtkt <rtkt@protonmail.com>
2023-06-08 17:56:44 +0700

    Updated AutoSSH settings on the desktop

--- desktop/autossh.nix
+++ desktop/autossh.nix
@@ -4,7 +4,7 @@
       user = "connector";
       name = "server-tunnel";
       monitoringPort = 51000;
-      extraArguments = "-N -i /var/lib/connector/.ssh/id_tunnel -R 50000:localhost:22 connector@45.90.57.45";
+      extraArguments = "-N -i /var/lib/connector/.ssh/id_tunnel -R 50000:localhost:22222 connector@45.90.57.45";
     }
   ];
 }

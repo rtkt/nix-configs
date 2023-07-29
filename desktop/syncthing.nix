c99f0f5e25315ef30c85395c0bc022d8edd39d8d
rtkt <rtkt@protonmail.com>
2023-07-29 19:15:14 +0700

    Updated Syncthing settings

--- desktop/syncthing.nix
+++ desktop/syncthing.nix
@@ -2,14 +2,11 @@
   services.syncthing = {
     enable = true;
     user = "rtkt";
-    dataDir = "/home/rtkt/Misc/Syncthing";
-    configDir = "/home/rtkt/.config/syncthing";
+    dataDir = "/home/rtkt/.config/syncthing/";
     overrideDevices = true;
     overrideFolders = true;
     settings = {
-      devices = {
-        "work" = {id = "6ZWYE5C-P3C5EBE-BAGAVOP-22IUEC4-JA7NMG3-L6MAUPH-64C4CV7-CWXQDAD";};
-      };
+      devices.work.id = "6ZWYE5C-P3C5EBE-BAGAVOP-22IUEC4-JA7NMG3-L6MAUPH-64C4CV7-CWXQDAD";
       folders = {
         "Keepass" = {
           path = "/home/rtkt/Документы/Keepass";

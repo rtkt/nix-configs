0f11b206c1954522b7091d9033f7870cdf40c2e0
rtkt <rtkt@protonmail.com>
2023-08-01 13:42:37 +0700

    Updated Syncthing settings

--- desktop/syncthing.nix
+++ desktop/syncthing.nix
@@ -6,19 +6,21 @@
     configDir = "/home/rtkt/.config/syncthing";
     overrideDevices = true;
     overrideFolders = true;
-    devices = {
-      "work" = {id = "6ZWYE5C-P3C5EBE-BAGAVOP-22IUEC4-JA7NMG3-L6MAUPH-64C4CV7-CWXQDAD";};
-    };
-    folders = {
-      "Keepass" = {
-        path = "/home/rtkt/Документы/Keepass";
-        type = "sendreceive";
-        devices = ["work"];
-        versioning = {
-          type = "staggered";
-          params = {
-            cleanInterval = "3600";
-            maxAge = "15768000";
+    settings = {
+      devices = {
+        "work" = {id = "6ZWYE5C-P3C5EBE-BAGAVOP-22IUEC4-JA7NMG3-L6MAUPH-64C4CV7-CWXQDAD";};
+      };
+      folders = {
+        "Keepass" = {
+          path = "/home/rtkt/Документы/Keepass";
+          type = "sendreceive";
+          devices = ["work"];
+          versioning = {
+            type = "staggered";
+            params = {
+              cleanInterval = "3600";
+              maxAge = "15768000";
+            };
           };
         };
       };

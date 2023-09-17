d4ebd3612838c456d6e0340ec3c72f09d689f975
rtkt <rtkt@protonmail.com>
2023-09-17 22:19:26 +0700

    Added CoreDNS on the desktop

--- desktop/network.nix
+++ desktop/network.nix
@@ -4,4 +4,14 @@
     interfaces.enp2s0.useDHCP = true;
     networkmanager.enable = true;
   };
+  services.coredns = {
+    enable = true;
+    config = ''
+      .:53 {
+        forward . https://45.90.57.45 {
+          tls_servername shitnsticks.top
+        }
+      }
+    '';
+  };
 }

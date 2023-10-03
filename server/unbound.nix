08426cede2959be49568835369437d1a1db9fb26
rtkt <rtkt@protonmail.com>
2023-10-03 21:09:23 +0700

    Disabled debug for the Unbound

--- server/unbound.nix
+++ server/unbound.nix
@@ -4,12 +4,12 @@
     settings = {
       server = {
         interface = [ "127.0.0.1" ];
-        verbosity = 4;
+        # verbosity = 4;
         "do-ip6" = false;
-        log-queries = true;
-        log-replies = true;
-        log-tag-queryreply = true;
-        log-servfail = true;
+        # log-queries = true;
+        # log-replies = true;
+        # log-tag-queryreply = true;
+        # log-servfail = true;
       };
     };
   };

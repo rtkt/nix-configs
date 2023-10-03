6d4cc01899d82e9be1cab3e1ad60cbff309ccfac
rtkt <rtkt@protonmail.com>
2023-10-03 20:22:01 +0700

    Deleted CoreDNS config on the desktop

--- desktop/network.nix
+++ desktop/network.nix
@@ -4,14 +4,4 @@
     interfaces.enp2s0.useDHCP = true;
     networkmanager.enable = true;
   };
-  services.coredns = {
-    enable = true;
-    config = ''
-      .:53 {
-        forward . https://45.90.57.45 {
-          tls_servername shitnsticks.top
-        }
-      }
-    '';
-  };
 }

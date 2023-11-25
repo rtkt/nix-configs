4aea4bd1a45f98c4900a4adcafcb52f47f0a0dd7
rtkt <rtkt@protonmail.com>
2023-11-25 15:16:50 +0700

    Deleted android flake input

--- flake.nix
+++ flake.nix
@@ -18,10 +18,6 @@
       url = "github:kamadorueda/alejandra/3.0.0";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    android = {
-      url = "github:tadfisher/android-nixpkgs";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
     nix-overlay = {
       url = "github:rtkt/nix-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -33,7 +29,6 @@
     nix-alien,
     nix-ld,
     alejandra,
-    android,
     sops-nix,
     nix-overlay,
   }: let

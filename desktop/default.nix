4ce34ed1f06c89f085c7cae826733fc6faaa5ac3
rtkt <rtkt@protonmail.com>
2024-02-02 14:18:15 +0700

    Reenabled common secrets

--- desktop/default.nix
+++ desktop/default.nix
@@ -5,6 +5,8 @@
   ...
 }: {
   imports = [
+    ./hosts.nix
+
     ./ananicy.nix
     ./autossh.nix
     ./boot.nix
---
+++ desktop/hosts.nix
@@ -0,0 +1,5 @@
+{
+  networking.hosts = {
+    "100.65.0.66" = [ "rtkt.cloud" "n8n.rtkt.cloud" "ntfy.rtkt.cloud" "files.rtkt.cloud" ];
+  };
+}

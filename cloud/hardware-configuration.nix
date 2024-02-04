1865ebf03a91a9a3a70b8b3ca98143a734c0a7f1
rtkt <rtkt@protonmail.com>
2024-02-04 23:04:03 +0700

    Added support for RAID

--- cloud/hardware-configuration.nix
+++ cloud/hardware-configuration.nix
@@ -11,6 +11,7 @@
     kernelModules = ["kvm-amd"];
     extraModulePackages = [];
     loader.grub.device = "/dev/disk/by-id/ata-Samsung_SSD_870_EVO_1TB_S74ZNX0W302758N";
+    swraid.enable = true;
   };
   fileSystems."/" = {
     device = "/dev/disk/by-uuid/80211811-0f30-403e-b992-4e65b4c27d53";

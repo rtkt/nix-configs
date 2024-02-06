d939661cf3190bd3636d255afc9c135fdcf91b0f
rtkt <rtkt@protonmail.com>
2024-02-06 20:58:32 +0700

    Enabled automatic garbage collection for all machines

--- common/defaults/nix.nix
+++ common/defaults/nix.nix
@@ -1,5 +1,9 @@
 {pkgs, ...}: {
   nix = {
+    gc = {
+      dates = "04:00";
+      options = "--delete-older-than 45d";
+    };
     extraOptions = ''
       experimental-features = nix-command flakes
     '';

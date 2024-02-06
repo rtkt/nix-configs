aafaa6d0a4aab3be69b3b4d204b3d73e81367582
rtkt <rtkt@protonmail.com>
2024-02-06 21:20:32 +0700

    Even more formatting

--- common/multimedia.nix
+++ common/multimedia.nix
@@ -6,6 +6,7 @@
   imports = [
     ./ananicy.nix
   ];
-  boot.kernelPackages = pkgs.linuxKernel.packages.linux_zen;
+  boot.kernelPackages =
+    pkgs.linuxKernel.packages.linux_zen;
   security.rtkit.enable = true;
 }

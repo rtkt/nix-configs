ab204788768c4f37013eca936dea2a48a45b84d2
rtkt <rtkt@protonmail.com>
2024-02-06 21:00:48 +0700

    Enabled automatic store optimisation for all machines

--- common/defaults/nix.nix
+++ common/defaults/nix.nix
@@ -4,6 +4,12 @@
       dates = "04:00";
       options = "--delete-older-than 45d";
     };
+    optimise = {
+      automatic = true;
+      dates = [
+        "04:40"
+      ];
+    };
     extraOptions = ''
       experimental-features = nix-command flakes
     '';

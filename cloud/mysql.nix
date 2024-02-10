4578b19471da58a1b4a32918e69562b69bfa1cea
rtkt <rtkt@protonmail.com>
2024-02-10 16:21:19 +0700

    Added mySQL backups support on the cloud

--- cloud/mysql.nix
+++ cloud/mysql.nix
@@ -1,6 +1,13 @@
-{pkgs, ...}: {
+{config, lib, pkgs, ...}: {
   services.mysql = {
     enable = true;
     package = pkgs.mariadb_106;
+  };
+  services.mysqlBackup = {
+    enable = true;
+    location = "/backups/mysql";
+    calendar = "Sat *-*-* 03:00:00";
+    databases = [ ]
+      ++ lib.optional (config.services.nextcloud.config.dbtype == "mysql") config.services.nextcloud.config.dbname;
   };
 }

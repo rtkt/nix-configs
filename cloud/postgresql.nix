e2c3bfff67e861d272db902ad22741d9a1a5c409
rtkt <rtkt@protonmail.com>
2024-02-13 12:56:35 +0700

    Added users and databases to the PostgreSQL

--- cloud/postgresql.nix
+++ cloud/postgresql.nix
@@ -2,6 +2,23 @@
 {
   services.postgresql = {
     enable = true;
+    enableTCPIP = true;
     package = pkgs.postgresql_16;
+    ensureDatabases = [
+      "n8n"
+    ];
+    ensureUsers = [
+      {
+        name = "root";
+        ensureClauses = {
+          superuser = true;
+          login = true;
+        };
+      }
+      {
+        name = "n8n";
+        ensureDBOwnership = true;
+      }
+    ];
   };
 }

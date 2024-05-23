540f21b28b04525c8fea8d235aee3b469d07e1a6
rtkt <rtkt@protonmail.com>
2024-05-23 18:58:13 +0700

    Added USB modules to the list of modules to load on boot

--- cloud/hardware.nix
+++ cloud/hardware.nix
@@ -2,7 +2,7 @@
   boot = {
     initrd.availableKernelModules = ["ahci" "ohci_pci" "ehci_pci" "xhci_pci" "usb_storage" "usbhid" "sd_mod"];
     initrd.kernelModules = [];
-    kernelModules = ["kvm-amd"];
+    kernelModules = ["kvm-amd" "hid_microsoft" "hid_generic" "usbhid"];
     extraModulePackages = [];
     swraid.enable = true;
   };

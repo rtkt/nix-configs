73257aaafadaf057d0f5febf191ece4bcd568908
rtkt <rtkt@protonmail.com>
2024-06-04 21:38:01 +0700

    Enabled printer sharing on the cloud

--- cloud/printing.nix
+++ cloud/printing.nix
@@ -1,8 +1,22 @@
 {pkgs, ...}: {
+  services.avahi = {
+    enable = true;
+    openFirewall = true;
+    nssmdns4 = true;
+    publish = {
+      enable = true;
+      userServices = true;
+    };
+  };
   services.printing = {
     enable = true;
     drivers = [pkgs.brlaser];
     stateless = true;
+    listenAddresses = ["*:631"];
+    allowFrom = ["all"];
+    browsing = true;
+    defaultShared = true;
+    openFirewall = true;
   };
   hardware.printers.ensurePrinters = [
     {
@@ -11,4 +25,5 @@
       model = "drv:///brlaser.drv/br2030.ppd";
     }
   ];
+  hardware.printers.ensureDefaultPrinter = "Brother_HL-2035R";
 }

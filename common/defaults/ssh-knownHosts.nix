5dcc91eb2c89d38a6e768898240c9b1fe391701f
rtkt <rtkt@protonmail.com>
2024-06-17 21:41:27 +0700

    Updated known SSH hosts

--- common/defaults/ssh-knownHosts.nix
+++ common/defaults/ssh-knownHosts.nix
@@ -1,8 +1,9 @@
 {
   programs.ssh.knownHosts = {
     "github.com".publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl";
-    "45.90.57.45".publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFJPyLHUwjWmsjZvwsR+kM6ic7fKrMiyneVSa5LNS+DW";
+    "107.173.134.188".publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKxPbt+6oI8kQSgrDFmDOUO2VrqbrtXtbhhlRBvjIK4+";
     desktop.publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH1Ewq8OqIKx8Gh3Nu4ZpdQxQVpZXFxVErYbaigjQjhu";
-    desktop.extraHostNames = ["[localhost]:50000" "([::1]:50000)"];
+    cloud.publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJkAvGcpWUYXcoEjMtpFSGuKXA2PTBg4vid1oiG6bN6A";
+    cloud.extraHostNames = ["[localhost]:50000" "([::1]:50000)"];
   };
 }

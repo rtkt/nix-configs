85c120696799b3b59138f517ae388cf06b491832
rtkt <rtkt@protonmail.com>
2024-06-20 23:26:05 +0700

    Cleaned up cloud SSH settings

--- cloud/ssh.nix
+++ cloud/ssh.nix
@@ -1,23 +1,2 @@
 {lib, ...}: {
-  services.openssh = {
-    # ports = [22222];
-    extraConfig = ''
-      GatewayPorts clientspecified
-    '';
-  };
-
-  programs.ssh.extraConfig = ''
-    Host localhost
-      ForwardAgent yes
-  '';
-
-  services.sshguard = {
-    blacklist_threshold = lib.mkForce 30;
-    detection_time = 4800;
-    whitelist = [
-      "45.57.90.45"
-      "185.91.179.79"
-      "195.209.97.211"
-    ];
-  };
 }

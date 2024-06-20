37ec9fc781b2ede4fcd328a36a77aa914cd5c0a7
rtkt <rtkt@protonmail.com>
2024-06-20 23:21:37 +0700

    Modified SSH security defaults

--- common/defaults/ssh.nix
+++ common/defaults/ssh.nix
@@ -25,17 +25,18 @@
     ipv4_subnet = 24;
     whitelist = [
       "185.91.179.79"
+      "100.65.0.19"
     ];
   };
   programs.ssh = {
     startAgent = true;
-    agentTimeout = "24h";
+    agentTimeout = "120h";
     knownHosts = {
       "github.com".publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl";
       "107.173.134.188".publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKxPbt+6oI8kQSgrDFmDOUO2VrqbrtXtbhhlRBvjIK4+";
       desktop.publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH1Ewq8OqIKx8Gh3Nu4ZpdQxQVpZXFxVErYbaigjQjhu";
       cloud.publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJkAvGcpWUYXcoEjMtpFSGuKXA2PTBg4vid1oiG6bN6A";
-      cloud.extraHostNames = ["[localhost]:50000" "([::1]:50000)"];
+      cloud.extraHostNames = ["[localhost]:50000" "([::1]:50000)" "n8n.rtkt.cloud" "files.rtkt.cloud"];
     };
   };
 }

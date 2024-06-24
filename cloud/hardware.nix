ae071b14c75c459f3bb002087038d2964663d76d
rtkt <rtkt@protonmail.com>
2024-06-24 22:21:03 +0700

    Added hardware package to the cloud

--- cloud/hardware.nix
+++ cloud/hardware.nix
@@ -1,4 +1,4 @@
-{
+{pkgs, ...}: {
   boot = {
     initrd.availableKernelModules = ["ahci" "ohci_pci" "ehci_pci" "xhci_pci" "usb_storage" "usbhid" "sd_mod"];
     initrd.kernelModules = [];
@@ -6,4 +6,5 @@
     extraModulePackages = [];
     swraid.enable = true;
   };
+  hardware.firmware = [pkgs.linux-firmware];
 }

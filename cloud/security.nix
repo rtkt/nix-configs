4d3671f3a0d74b5a45a9e57628be06f757def150
rtkt <rtkt@protonmail.com>
2024-06-26 19:51:41 +0700

    Switched to doas and blocked sudo

--- cloud/security.nix
+++ cloud/security.nix
@@ -3,22 +3,38 @@
   pkgs,
   ...
 }: {
-  security.pam.loginLimits = [
-    {
-      domain = "*";
-      item = "nproc";
-      type = "hard";
-      value = 5000;
-    }
-    {
-      domain = "*";
-      item = "nproc";
-      type = "soft";
-      value = 1000;
-    }
-  ];
+  security = {
+    allowSimultaneousMultithreading = true;
+    sudo.enable = lib.mkForce false;
+    doas = {
+      enable = true;
+      wheelNeedsPassword = true;
+      extraRules = [
+        {
+          users = ["rtkt"];
+          noPass = false;
+          keepEnv = true;
+          persist = false;
+          noLog = false;
+        }
+      ];
+    };
+    pam.loginLimits = [
+      {
+        domain = "*";
+        item = "nproc";
+        type = "hard";
+        value = 5000;
+      }
+      {
+        domain = "*";
+        item = "nproc";
+        type = "soft";
+        value = 1000;
+      }
+    ];
+  };
   # environment.memoryAllocator.provider = "libc";
-  security.allowSimultaneousMultithreading = true;
   boot.kernelPackages = lib.mkForce pkgs.linuxPackages_hardened;
   boot.loader.systemd-boot.enable = lib.mkForce false;
   boot.lanzaboote = {

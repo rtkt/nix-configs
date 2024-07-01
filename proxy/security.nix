e52df5c64eea8ecdcc6c9ff3ae87a4e5e27a8f29
rtkt <rtkt@protonmail.com>
2024-07-01 23:41:27 +0700

    Updated security on the proxy

--- proxy/security.nix
+++ proxy/security.nix
@@ -1,19 +1,44 @@
 {
-  environment.variables = {
-    TMOUT = "600";
+  lib,
+  pkgs,
+  ...
+}: {
+  security = {
+    pam.loginLimits = [
+      {
+        domain = "*";
+        item = "nproc";
+        type = "hard";
+        value = 1000;
+      }
+      {
+        domain = "*";
+        item = "nproc";
+        type = "soft";
+        value = 200;
+      }
+    ];
+    allowSimultaneousMultithreading = true;
+    sudo.enable = lib.mkForce false;
+    doas = {
+      enable = true;
+      wheelNeedsPassword = true;
+      extraRules = [
+        {
+          users = ["rtkt"];
+          noPass = false;
+          keepEnv = true;
+          persist = false;
+          noLog = false;
+        }
+      ];
+    };
+    pam.services.su = {
+      requireWheel = lib.mkForce true;
+      rootOK = lib.mkForce false;
+    };
   };
-  security.pam.loginLimits = [
-    {
-      domain = "*";
-      item = "nproc";
-      type = "hard";
-      value = 1000;
-    }
-    {
-      domain = "*";
-      item = "nproc";
-      type = "soft";
-      value = 200;
-    }
-  ];
+  # environment.memoryAllocator.provider = "libc";
+  boot.kernelPackages = lib.mkForce pkgs.linuxPackages_hardened;
+  nix.settings.allowed-users = lib.mkForce ["root"];
 }

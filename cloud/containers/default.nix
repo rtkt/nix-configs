bcb446914469d08c09234deefc03c0ef3ae8967d
rtkt <rtkt@protonmail.com>
2024-07-13 21:26:29 +0700

    Archived old cloud/containers folder and started creating it from scratch

--- cloud/containers/default.nix
+++ cloud/containers/default.nix
@@ -1,7 +1,10 @@
 {
-  config,
-  lib,
-  pkgs,
-  ...
-}: {
+  networking.firewall.allowedTCPPorts = [9000];
+  virtualisation.oci-containers.containers.nextcloud = {
+    image = "nextcloud/all-in-one:latest";
+    hostname = "nextcloud";
+    ports = [
+      "8443:9000"
+    ];
+  };
 }

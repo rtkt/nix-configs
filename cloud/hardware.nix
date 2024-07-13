2434e640c881a00719e55e5ad3a725a79e3ca283
rtkt <rtkt@protonmail.com>
2024-07-13 15:03:20 +0700

    Added xt_conntrack module and support for vfat

--- cloud/hardware.nix
+++ cloud/hardware.nix
@@ -2,13 +2,12 @@
   boot = {
     initrd.availableKernelModules = ["ahci" "ohci_pci" "ehci_pci" "xhci_pci" "usb_storage" "usbhid" "sd_mod"];
     initrd.kernelModules = [];
-    kernelModules = ["kvm-amd" "hid_microsoft" "hid_generic" "usbhid" "xxhash_generic"];
+    kernelModules = ["kvm-amd" "hid_microsoft" "hid_generic" "usbhid" "xt_conntrack"];
     extraModulePackages = [];
     supportedFilesystems = {
-      btrfs = true;
+      vfat = true;
       zfs = true;
     };
-    # swraid.enable = true;
   };
   services.irqbalance.enable = false;
   networking.hostId = "2aa4624b";

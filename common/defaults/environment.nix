95837fd879ad655ebdeb587ef7f42405318c21a7
rtkt <rtkt@protonmail.com>
2024-07-17 01:16:39 +0700

    1

--- common/defaults/environment.nix
+++ common/defaults/environment.nix
@@ -27,39 +27,36 @@
   };
   programs.zsh.enable = lib.mkDefault true;
   users.defaultUserShell = lib.mkOverride 900 pkgs.zsh;
-  programs.zsh.shellAliases =
-    {
-      ssh-agent = "ssh-agent -t ${config.programs.ssh.agentTimeout}";
-      list-generations = "nix-env -p /nix/var/nix/profiles/system --list-generations";
-      rb = "nixos-rebuild ";
-      rbb = "nixos-rebuild boot";
-      rbt = "nixos-rebuild test";
-      rbs = "nixos-rebuild switch";
-      G = " | grep";
-      H = " | head";
-      T = " | tail";
-      L = " | less";
-      c = "cat";
-      s = "systemctl";
-      scat = "systemctl cat";
-      sst = "systemctl status";
-      sres = "systemctl restart";
-      j = "journalctl";
-      jb = "journalctl -xb";
-      je = "journalctl -xe";
-    }
-    // (lib.mkIf (! config.boot.isContainer) {
-      l = "ls -lha";
+  programs.zsh.shellAliases = {
+    ssh-agent = "ssh-agent -t ${config.programs.ssh.agentTimeout}";
+    list-generations = "nix-env -p /nix/var/nix/profiles/system --list-generations";
+    rb = "nixos-rebuild ";
+    rbb = "nixos-rebuild boot";
+    rbt = "nixos-rebuild test";
+    rbs = "nixos-rebuild switch";
+    G = " | grep";
+    H = " | head";
+    T = " | tail";
+    L = " | less";
+    c = "cat";
+    s = "systemctl";
+    scat = "systemctl cat";
+    sst = "systemctl status";
+    sres = "systemctl restart";
+    j = "journalctl";
+    jb = "journalctl -xb";
+    je = "journalctl -xe";
+    l = "ls -lha";
 
-      g = "git";
-      ga = "git add";
-      gc = "git commit -v";
-      gca = "git commit --amend -v";
-      gup = "git push";
-      gdown = "git pull";
-      gs = "git status";
-      gl = "git log";
-      dev = "nix develop -c $SHELL";
-      m = "micro";
-    });
+    g = "git";
+    ga = "git add";
+    gc = "git commit -v";
+    gca = "git commit --amend -v";
+    gup = "git push";
+    gdown = "git pull";
+    gs = "git status";
+    gl = "git log";
+    dev = "nix develop -c $SHELL";
+    m = "micro";
+  };
 }

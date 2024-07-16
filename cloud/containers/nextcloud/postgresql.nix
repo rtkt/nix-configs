8f70e13cfc18fd9e7f557fc09e85c285ff641ef2
rtkt <rtkt@protonmail.com>
2024-07-16 22:50:33 +0700

    Added 'ensure' settings to PostgreSQL

--- cloud/containers/nextcloud/postgresql.nix
+++ cloud/containers/nextcloud/postgresql.nix
@@ -1,7 +1,13 @@
-{pkgs, ...}: {
+{
+  config,
+  pkgs,
+  ...
+}: {
   services.postgresql = {
     enable = true;
     enableJIT = true;
+    ensureDatabases = [config.services.nextcloud.config.dbname];
+    ensureUsers = [{name = config.services.nextcloud.config.dbuser;}];
     package = pkgs.postgresql_16_jit;
     settings = {
       full_page_writes = false;
